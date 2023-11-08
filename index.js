110b7a69000a269e1e2cff789fb3df6397a086b3
Jefferson C <jcherton@hotmail.com>
2023-11-07 20:14:21 -0600

    testing CORS again

--- index.js
+++ index.js
@@ -1,13 +1,24 @@
 const express = require("express");
 const app = express();
+const cors = require('cors');
+const port = process.env.PORT || 3000;
 
 //https://www.section.io/engineering-education/how-to-use-cors-in-nodejs-with-express/
 // const cors = require('cors');
-// app.use(cors())
 
+// Enable CORS for specific routes.
+app.use(cors());
+
+// Serve static files from the 'client' directory.
 app.use(express.static(__dirname + '/client'))
 
-const port = process.env.PORT || 3000
+// Serve your JSON file with CORS headers.
+app.get('/json-data', (req, res) => {
+	// Replace 'your-json-file.json' with the actual path to your JSON file.
+	res.sendFile(__dirname + '/your-json-file.json');
+  });
+
+
 app.get('/test', function(request, response) {
 	response.type('text/plain')
 	response.send('Node.js and Express running on port='+port)
