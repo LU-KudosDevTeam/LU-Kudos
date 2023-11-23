ab93535e3359f8447abb5016539d2090052aef6a
Jefferson C <jcherton@hotmail.com>
2023-11-23 01:03:10 -0600

    MVP v1 is effectively complete.
    Unfortunately, the email address chosen is not whitelisted,
    therefore it will land in the spam folder. Can't beat that, for now.

--- index.js
+++ index.js
@@ -1,13 +1,16 @@
-const express = require("express");
+const express = require("express");			// needed as middleware to serve and route files
 const app = express();
-const cors = require('cors');
-const port = process.env.PORT || 3000;
-const nodemailer = require('nodemailer');
-const fs = require('fs');
+
+const cors = require('cors');				// needed to pull resources (like JSON) from anywhere in the server to anywhere in the server
+
+const port = process.env.PORT || 3000; 		// sets up a port and uses it
+
+const nodemailer = require('nodemailer');  	// needed for email
+
+require('dotenv').config(); 				// needed to use environment variables
 
 
 //https://www.section.io/engineering-education/how-to-use-cors-in-nodejs-with-express/
-// const cors = require('cors');
 
 // Enable CORS for specific routes.
 app.use(cors());
@@ -24,62 +27,79 @@ app.get('/test', function(request, response) {
 })
 
 app.listen(port, function() {
-	console.log("Server is running at http://localhost:3000/")
+	console.log(`Server is running at http://localhost:${port}/`)
 })
 
 
-app.post('/send-email', async (req, res) => { // modified with the help of ChatGPT 3.5 and https://www.w3schools.com/nodejs/nodejs_email.asp
+app.post('/send-email', async (req, res) => { // achieved with the help of ChatGPT 4.0
+    try {
+        const jsonData = req.body; // Received JSON data from the client
+        await sendMail(jsonData);
+        res.status(200).send('Email sent successfully');
+    } catch (error) {
+        console.error('Error:', error);
+        res.status(500).send('Error sending email');
+    }
+});
 
-	email_acctname = 'sender@gmail.com'
-	email_password = readPassword()
-	email_target = 'example@example.com'
+async function sendMail(jsonData) {
+	const email_acctname = process.env.EMAIL_ACCOUNT;
+    const email_password = process.env.EMAIL_PASSWORD;	// needed to use an app password for a Gmail account via MFA
 
-	try {
-		const jsonData = req.body; // Received JSON data from the client
-	}
-	catch
-	{
-		console.log('could not find body.')
-	}
+    var transporter = nodemailer.createTransport({
+        service: 'gmail',
+        host: "smtp.gmail.com",
+        port: 587,
+        secure: false,
+        auth: {
+            user: email_acctname,
+            pass: email_password
+        },
+		debug: true,
+    });
 
-	var transporter = nodemailer.createTransport({
-		service: 'gmail',
-		host: "smtp.gmail.com",
-		port: 587,
-		secure: false,
-		auth: {
-		  user: email_acctname,
-		  pass: email_password
-		}
-	  });
-	  
-	  var mailOptions = {
-		from: email_acctname,
-		to: email_target,
-		subject: 'JSON Data from Client',
-        text: "help"
-		//JSON.stringify(jsonData) // Convert JSON to string for email body
-	  };
-	  
-	  	try {
-			// Send email
-			await transporter.sendMail(mailOptions);
-			res.status(200).send('Email sent successfully');
-		} catch (error) {
-			res.status(500).send('Error sending email');
-			console.log
-		}
+    var mailOptions = {
+        from: `LU Kudos ${email_acctname}`,
+        to: jsonData.selected.email,
+        subject: 'LU Kudos - Congratulations on your Nomination!',
 
+        //text: JSON.stringify(jsonData) // Convert JSON to string for email = old but may need a fallback like this.
 
-})
+		// this hardcoded HTML will be changed in the future 
+		// to use a different file that takes in the JSON 
+		// and spits out the right HTML + CSS
+		html: `	
+		<body style = "text-align: center;">
+		<h1 style = "font-size: 5vmin;">
+			${jsonData.namedval}
+		</h1>
+		
+		<p>
+			You have been nominated for your demonstration of ${jsonData.namedval} at Lewis University.
+			
+			<br></br>
+			
+			This was why they nominated you:
+		</p>
+		
+		<div style = "font-style: italic;">
+			${jsonData.details}
+		</div>
+	
+		<footer style = "font-size: 8px;">
+			this email has been sent to you via the LU Kudos team. <br></br>
+			<span style = "font-size: 6px;"> Reference ID: ${jsonData.finalID} </span>
+		</footer>
+		</body>
+		
+		`
+    };
 
-function readPassword() {
-	fs.readFile('PASSWORD.log', 'utf8', (err, data) => {
-		if (err) {
-		  console.error(err);
-		  return;
-		}
-		//console.log(data);
-		return data
-	  });
+    try {
+        await transporter.sendMail(mailOptions);
+        console.log("Email sent successfully");
+    } catch (error) {
+        console.error("Error sending mail:", error);
+        // throw error; // Rethrow to handle it in the endpoint
+    }
 }
